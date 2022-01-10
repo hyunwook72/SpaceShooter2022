0707e1c42e96db6671f8d72cf8465e3b041eebae
Lee Jae Hyun <myleje@gmail.com>
2022-01-10 14:32:24 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -2,19 +2,6 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    // 가장 먼저 호출, 1번 호출
-    // 데이터 초기화
-    void Awake()
-    {
-
-    }
-
-    // 해당 클래스(스크립트)가 활성화 될때마다 호출
-    void OnEnable()
-    {
-
-    }
-
     // 1회 호출
     void Start()
     {
@@ -24,18 +11,7 @@ public class PlayerCtrl : MonoBehaviour
     // 매 프레임 마다 호출, 화면을 랜더링하는 주기
     void Update()
     {
-
-    }
-
-    // 호출간격이 규칙적 (0.02 초), Box2D, Physic(3D)
-    void FixedUpdate()
-    {
-
-    }
-
-    // Update에서 처리된 계산 결괏값을 참조해서 후처리 작업을 할때
-    void LateUpdate()
-    {
-
+        //transform.position += new Vector3(0, 0, 0.1f);
+        transform.Translate(Vector3.forward * 0.1f);
     }
 }
