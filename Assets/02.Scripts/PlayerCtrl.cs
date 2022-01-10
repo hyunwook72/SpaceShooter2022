3301843a41612b201b217390bad2948626f1aa11
Lee Jae Hyun <myleje@gmail.com>
2022-01-10 15:34:40 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -4,6 +4,7 @@ public class PlayerCtrl : MonoBehaviour
 {
     private float v;
     private float h;
+    private float r;
 
     // 1회 호출
     void Start()
@@ -16,8 +17,7 @@ public class PlayerCtrl : MonoBehaviour
     {
         v = Input.GetAxis("Vertical"); //Up, Down, W, S // -1.0f ~ 0.0f ~ +1.0f
         h = Input.GetAxis("Horizontal");// -1.0f ~ 0.0f ~ +1.0f
-        Debug.Log("v=" + v); // 콘솔 뷰에 메시지 출력
-        Debug.Log("h=" + h); // 콘솔 뷰에 메시지 출력
+        r = Input.GetAxis("Mouse X");
 
         // transform.Translate(방향 * 속도 * 변위)
         // transform.Translate(Vector3.forward * 0.1f * v); //전진/후진
@@ -25,9 +25,14 @@ public class PlayerCtrl : MonoBehaviour
 
         // 벡터의 덧셈 연산
         // Vector3 moveDir = (전후진벡터) + (좌우벡터)
+        //이동처리
         Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
 
         transform.Translate(moveDir.normalized * 0.1f);
+
+        //회전처리
+        transform.Rotate(Vector3.up * 80.0f * r);
+
         //Debug.Log("정규화 이전 벡터 =" + moveDir.magnitude);//정규화 이전의 벡터 크기
         //Debug.Log("정규화 벡터 =" + moveDir.normalized.magnitude); //정규화 벡터
     }
